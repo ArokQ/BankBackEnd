52888b766bd4faa21c46bde2294dcf84f8039c0e
Heiss <heiss86@live.dk>
2014-03-13 11:27:07 +0100

    Tables opdateret

--- src/conf/tables.sql
+++ src/conf/tables.sql
@@ -6,19 +6,19 @@ drop table CheckingAccount;
 
 ---------------
 create table Person(
-cpr int primary key,
+cpr varchar(12) primary key,
 title varchar(40) not null,
 firstName varchar(40) not null,
 lastName varchar(40) not null,
 street varchar(100) not null,
 phone int not null,
 email varchar(80),
-password varchar(80));
-
+password varchar(80)
+postal_code varchar(8) not null references Postal(code));
 
 create table Postal(
-code int primary key,
-district varchar(40));
+code varchar(8) primary key,
+district varchar(80) not null);
 
 -- FOREIGN KEYS!???????????
 -----------------
@@ -26,15 +26,15 @@ district varchar(40));
 create table Account(
 accountNumber int primary key,
 balance float not null,
-interest float not null);
+interest float not null,
+person_cpr varchar(12) not null references Person(cpr));
 
 create table Transfer(
 amount float primary key,
-transferDate date not null);
+transferDate date not null,
+targetAccount varchar(10) not null,
+account_accountNumber int not null references Account(accountNumber));
 
-create table CheckingAccount(
-cpr int not null,
-interest float not null);
 
 
 ------------------
